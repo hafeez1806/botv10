6a84057677e35e4aae6417d35b755f96408042d6
hafeez1806 <60182242+hafeez1806@users.noreply.github.com>
2020-02-03 11:26:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages?EAALDLMirOvEBANQZB3QtT
 VERIFY_TOKEN = 'hafeezkhan1806'
 PAGE_ACCESS_TOKEN = 'EAALDLMirOvEBANQZB3QtTFMlmxwuOi6XYvO06tqDgkl72xbjQptFgVoHHGWR5etfHc6ow7DwkmBrq2vNTnZCCeuWXusa852lOTc8iZAeVZAiRIZAxXgui2xouUvNV1mhClQbvG98AKrrQBZATO5tsEaeZCsKeAj5Jdot1G2DbU7ajwCr8dCPaEq'
 
+def remove(string): 
+ return string.replace(" ", "")
 
 def get_bot_response(message):
- input = message
+ input = remove(message)
  with open("data.json", "r") as jsonFile: #open file
   data = json.load(jsonFile)
  if input in data:
@@ -19,8 +21,6 @@ def get_bot_response(message):
  else:
   return("sorry i cant repy to that yet") 
    
- 
-
 
 def verify_webhook(req):
  if req.args.get("hub.verify_token") == VERIFY_TOKEN:
@@ -44,7 +44,6 @@ def send_message(recipient_id, text):
  if "help" in text:
   payload = {
         'message': {
-            "text": "How can i help you?",
             "quick_replies":[
               {
                "content_type":"text",
